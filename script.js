206eff3368ad7b605c01772d7348cba571590bf9
Christian Ramirez <christian.r0602@gmail.com>
2022-06-27 10:28:00 -0500

    three new instances

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ class User {
   }
 
   displayBalance(){
-    console.log(this.name + this.accountBalance)
+    console.log(this.name +' has ' + this.accountBalance + ' dollars in the bank')
   }
 }
 
 const christian = new User('Christian', 'christian_ramirez@javascript.com')
+const daniela = new User('Daniela', 'daniela_ramirez@javascript.com')
+const ana = new User('Ana', 'ana_reyes@javascript.com')
 christian.makeDeposit(1000)
 christian.makeWithdrawal(50)
-console.log(christian.accountBalance)
+console.log(daniela.accountBalance)
+console.log(christian.displayBalance())
